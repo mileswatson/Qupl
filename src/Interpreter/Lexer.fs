bcf82629e235de7aaff49d781aac1f58808e7d68
Miles Watson <contact@mileswatson.net>
2021-03-30 20:43:38 +0100

    Added basic token types.

--- src/Interpreter/Lexer.fs
+++ src/Interpreter/Lexer.fs
@@ -1,5 +1,25 @@
 namespace Interpreter
 
+type Keyword =
+    | Let
+    | Funq
+    | Log
+
+type Gate =
+    | H
+    | CNOT
+    | NOT
+    | Identifier of string
+
+type Token =
+    | Keyword of Keyword
+    | Gate of Gate
+    | Equal
+    | Colon
+    | OpenBracket
+    | CloseBracket
+    | Identifier of string
+
 module Lexer =
 
     let splitLines (code: string) =
